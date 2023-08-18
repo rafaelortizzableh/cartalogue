aaece05ebd10d1180ac65f753f7a08b6aea89a34
Rafael Ortiz Zableh <57945332+rafaelortizzableh@users.noreply.github.com>
2023-08-18 18:56:31 +0200

    feat: add placeholder home screen

---
+++ lib/features/home/ui/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../features.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  static const routeName = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cartalogue'),
+        actions: const [_ThemeLinkIcon()],
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
+
+class _ThemeLinkIcon extends ConsumerWidget {
+  const _ThemeLinkIcon();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeControllerProvider);
+    final icon = _assignIcon(themeMode);
+
+    return IconButton(
+      icon: Icon(icon),
+      onPressed: () => context.push(ThemeScreen.routeName),
+    );
+  }
+
+  IconData _assignIcon(ThemeMode themeMode) {
+    switch (themeMode) {
+      case ThemeMode.system:
+        return Icons.brightness_auto;
+      case ThemeMode.light:
+        return Icons.brightness_high;
+      case ThemeMode.dark:
+        return Icons.brightness_3;
+    }
+  }
+}
