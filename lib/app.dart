a7b7f5e45d2d7ee13248136c21ac35affa277c22
Rafael Ortiz Zableh <57945332+rafaelortizzableh@users.noreply.github.com>
2023-08-18 18:54:26 +0200

    chore: add testing helpers

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'core/core.dart';
+import 'features/features.dart';
 
 class CartalogueApp extends ConsumerWidget {
   const CartalogueApp({super.key});
@@ -9,9 +10,12 @@ class CartalogueApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
-
+    final theme = ref.watch(themeControllerProvider);
     return MaterialApp.router(
       title: 'Cartalogue',
+      theme: CustomTheme.lightTheme(),
+      darkTheme: CustomTheme.darkTheme(),
+      themeMode: theme,
       routeInformationProvider: router.routeInformationProvider,
       routeInformationParser: router.routeInformationParser,
       routerDelegate: router.routerDelegate,
---
+++ test/helpers/mocks.dart
@@ -0,0 +1,7 @@
+import 'package:cartalogue/core/core.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockLoggerService extends Mock implements LoggerService {}
+
+class MockSharedPreferencesService extends Mock
+    implements SharedPreferencesService {}
